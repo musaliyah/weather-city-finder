7785228118ab087acb3ddcc3a29ab3f3b1447c46
musaliyah <asmusaliar@gmail.com>
2022-09-08 18:48:35 -0700

    reformulating script file

--- script.js
+++ script.js
@@ -1,21 +1,93 @@
-var key ="544a9000f5fa302a2a222c09fef201ce";
+var apiKey ='7b0cb6b05833e4f392297ec0ca158aea';
+var date = moment().format('MMMM Do YYYY, h:mm:ss a')
+var city = "Kochi"
+var pastSearches = [];
 
-searchedCities();
-$("#display-forecast").addClass('hide');
-$("#forecast-time").addClass('hide');
+$('.search').on("click", function(event) {
+    event.preventDefault();
+    cityInput = $(this).parent('.srchBtn').siblings('.textVal').val().trim();
+    if (cityInput === "") {
+        return;
+    };
+    pastSearches.push(city);
+    //localStorage
+    //empty
+    //history
+    //weathertoday
+
+})
+// var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='
+// var weatherIconsUrl = 'http://openweathermap.org/img/wn/'
+// var callUrl = 'https://api.openweathermap.org/data/2.5/onecall?at='
 
 
-if(localStorage.getItem('last_searched') == null){
 
-    $('#weather-forecast').addClass('hide align-middle');
-    $('#searched-cities').addClass('hide');
-}else{
-    newForecast()
-    var interval = setInterval(newForecast,600000);
-}
+// function getWeather(city){
+//     var weatherApiUrl = weatherUrl + city + '&appid=' + apiKey;
 
-function newForecast(){
-    $('#forecast-time').addClass('hide');
-    $('#display-forecast').removeClass('hide');
+//     fetch(weatherApiUrl).then(function (geoLocation) {
+//         if (geoLocation.ok){
+//             geoLocation.json().then(function (data){
+//                 var latitude = data.coord.lat;
+//                 var longitude = data.coord.lon;
+
+//                 var callApiUrl = callUrl + latitude + longitude + '&appid=' + apiKey + '&units=imperial';
+
+//                 fetch(callApiUrl).then(function (weatherData){
+//                     if(weatherData.ok) {
+//                         weatherData.json().then(function(weatherData){
+//                             var presentWeatherEl = $('<div>').attr({id: 'present-weather'})
+//                         })
+//                         var weatherSymbol = weatherData.curren.weather[0].icon;
+//                         var presentWeatherIcon = weatherIconsUrl + weatherSymbol + '.png';
+
+//                         var boxHeading = $('<h2>').text(city + '('+ currentDay + ')')
+
+//                         var iconImage = $('<img>').attr({
+//                             id: 'present-weather-icon',
+//                             src: presentWeatherIcon,
+//                             alt: 'Displays Weather Symbols'
+//                         })
+
+//                         var presentWeatherListEl = $('<ul>')
+//                         var weatherFactors = ['Temp: ' + weatherData.current.temp + ' F', 'Wind ']
+//                     }
+//                 })
+//             }
+//         });
+// }
+
+// function searchHistory (city){
+
+//     var displayPastSearch = $('<button>'),addClass('btn').text(city).on('click', function(){
+//         $('#current-weather').remove();
+//         $('#five-days-title').remove();
+//         $("five-days").empty();
+//         displayWeather(city);
+//     })
+//     .attr({
+//         type: 'button'
+//     });
+
+//     userSearchEl.append(displayPastSearch);
+
+// }
+
+// function pastSearches() { 
+//     var storedCities = JSON.parse(localStorage.getItem('search history'));
     
-}
+//     if (!storedCities) { 
+//         storedCities = {
+//             citySearch: []
+        
+//         };
+// } else  {
+//     for (var i=0; i< pastSearches.searchedCity.length; i++){
+//         searchHistory(storedCities.citySearch[i]);
+//     }
+// }
+
+// return storedCities;
+
+// } console.log(storedCities);
+
